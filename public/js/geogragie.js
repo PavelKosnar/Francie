5dcb9fa07ae30ee7a4ebc31b6d97fa2885e9d7d4
PavelKosnar <kosnarpaveljr@gmail.com>
2021-06-18 20:15:45 +0200

    Rest api

--- public/js/geogragie.js
+++ public/js/geogragie.js
@@ -1,107 +1,54 @@
 
 $(function() {
 
-    function mestaBlock(mesta) {
-        $('rect').on('click', function () {
-            $('.mesta').fadeOut(800);
-            $('rect').css('fill', '#0045ff');
-            $(this).css('fill', 'red');
-            let id = $(this).attr('id');
-            let podrobnosti = mesta.find(item => {return item.id == id});
-            $('.mesta').fadeIn(800, function() {
-                $('.mesta').html(`
-                        <h1 class="mesta-jmeno">${podrobnosti.jmeno}</h1>
-                        <p class="mesta-text">${podrobnosti.text}</p>
-                        <p>Více zde: <a href="${podrobnosti.odkaz}">${podrobnosti.odkaz}</a></p>
-                        <img src="../img/${podrobnosti.obrazek}" alt="${podrobnosti.jmeno}">
-                `);
-            });
-        });
-    }
-    
-    fetch('../data/mesta.json')
-   .then(response => {
-      console.log(response);
-      return response.json()
-   })
-   .then(json => {
-      console.log(json);
-      mestaBlock(json);
-   })
-   .catch(function (error) {
-      console.error('Chyba: \n', error);
-   });
-
-/*
-   function regionyBlock(regiony) {
-        $('path').on('click', function () {
-            $('path').removeClass('aktivniPath');
-            $(this).css('fill', '#333');
-            $('.regiony').hide(800);
-            let id = $(this).attr('id');
-            let podrobnosti = regiony.find(item => {return item.id == id});
-            $('.regiony').show(800, function() {
-                $('.regiony').html(`
-                    <ul class="${podrobnosti.id}">
-                        <li class="regiony-jmeno">${podrobnosti.jmeno}</li>
-                        <li class="regiony-text">${podrobnosti.text}</li>
-                    </ul>
-                `);
-            });
-        });
-        $('path').on('mouseover', function() {
-            $('path').css('fill', puvodniPath);
-            $(this).css('fill', '#555');
-        });
-        $('path').on('mouseleave', function() {
-            $(this).css('fill', puvodniPath);
-        });
-    };
-
-    fetch('../data/regiony.json')
-    .then(response => {
-    console.log(response);
-    return response.json()
-    })
-    .then(json => {
-    console.log(json);
-    regionyBlock(json);
-    })
+    let mesta = [];
+    fetch('http://localhost:8080/api/mesta')
+    .then(response => { return response.json() })
+    .then(json => { mesta = json; })
     .catch(function (error) {
-    console.error('Chyba: \n', error);
+       console.error('Chyba: \n', error);
     });
 
-*/
+    let pamatky = [];
+    fetch('http://localhost:8080/api/pamatky')
+    .then(response => { return response.json() })
+    .then(json => { pamatky = json; })
+    .catch(function (error) {
+       console.error('Chyba: \n', error);
+    });
 
-    function pamatkyBlock(pamatky) {
-        $('ellipse').on('click', function () {
-            $('.pamatky').fadeOut(800);
-            $('ellipse').css('fill', 'yellow');
-            $(this).css('fill', 'green');
-            let id2 = $(this).attr('id');
-            let podrobnosti2 = pamatky.find(item => {return item.id == id2});
-            $('.pamatky').fadeIn(800, function() {
-                $('.pamatky').html(`
-                        <h1 class="pamatky-jmeno">${podrobnosti2.jmeno}</h1>
-                        <p class="pamatky-text">${podrobnosti2.text}</p>
-                        <p>Více zde: <a href="${podrobnosti2.odkaz}">${podrobnosti2.odkaz}</a></p>
-                        <img src="../img/${podrobnosti2.obrazek}" alt="${podrobnosti2.jmeno}">
-                `);
-            });
+
+    $('rect').on('click', function () {
+        $('.mesta').fadeOut(800);
+        $('rect').css('fill', '#0045ff');
+        $(this).css('fill', 'red');
+        let id = $(this).attr('id');
+        let podrobnosti = mesta.find(item => {return item.id == id});
+        $('.mesta').fadeIn(800, function() {
+            $('.mesta').html(`
+                    <h1 class="mesta-jmeno">${podrobnosti.jmeno}</h1>
+                    <p class="mesta-text">${podrobnosti.text}</p>
+                    <p>Více zde: <a href="${podrobnosti.odkaz}">${podrobnosti.odkaz}</a></p>
+                    <img src="../img/${podrobnosti.obrazek}" alt="${podrobnosti.jmeno}">
+            `);
         });
-    }
+    });
+    
 
+    $('ellipse').on('click', function () {
+        $('.pamatky').fadeOut(800);
+        $('ellipse').css('fill', 'yellow');
+        $(this).css('fill', 'green');
+        let id1 = $(this).attr('id');
+        let podrobnosti = pamatky.find(item => {return item.id == id1});
+        $('.pamatky').fadeIn(800, function() {
+            $('.pamatky').html(`
+                    <h1 class="pamatky-jmeno">${podrobnosti.jmeno}</h1>
+                    <p class="pamatky-text">${podrobnosti.text}</p>
+                    <p>Více zde: <a href="${podrobnosti.odkaz}">${podrobnosti.odkaz}</a></p>
+                    <img src="../img/${podrobnosti.obrazek}" alt="${podrobnosti.jmeno}">
+            `);
+        });
+    });
 
-    fetch('../data/pamatky.json')
-   .then(response => {
-      console.log(response);
-      return response.json()
-   })
-   .then(json => {
-      console.log(json);
-      pamatkyBlock(json);
-   })
-   .catch(function (error) {
-      console.error('Chyba: \n', error);
-   });
 })
--- server.js
+++ server.js
@@ -1,11 +1,52 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const fsPromises = require("fs").promises;
+
+async function readJSON(path) {
+  const data = await fsPromises
+    .readFile(path, 'utf-8')
+    .catch(err => console.error("Failed to read file", err));
+  return JSON.parse(data.toString());
+}
+
+async function writeJSON(path, data) {
+    data = JSON.stringify(data);
+    await fsPromises
+      .writeFile(path, data, 'utf-8')
+      .catch(err => console.error("Failed to write file", err));
+    return data;
+}
 
 const app = express();
 const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/api/udalosti", (req, res) => {
+    readJSON('public/data/udalosti.json')
+    .then(data => res.send(data))
+    .catch(err => res.send('Soubor nebylo možné načíst', err));       
+});
+
+app.get("/api/osobnosti", (req, res) => {
+    readJSON('public/data/osobnosti.json')
+    .then(data => res.send(data))
+    .catch(err => res.send('Soubor nebylo možné načíst', err));       
+});
+
+app.get("/api/mesta", (req, res) => {
+    readJSON('public/data/mesta.json')
+    .then(data => res.send(data))
+    .catch(err => res.send('Soubor nebylo možné načíst', err));       
+});
+
+app.get("/api/pamatky", (req, res) => {
+    readJSON('public/data/pamatky.json')
+    .then(data => res.send(data))
+    .catch(err => res.send('Soubor nebylo možné načíst', err));       
+});
+
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
